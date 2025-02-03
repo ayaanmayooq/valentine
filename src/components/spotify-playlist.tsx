3b360c0c6f0e1325636e979f04745de71f94a6b0
Sai <byrrajus@msu.edu>
2025-02-03 14:16:46 -0500

    Scroll Bar fix and Spotify fix

--- src/components/spotify-playlist.tsx
+++ src/components/spotify-playlist.tsx
@@ -1,16 +1,45 @@
-export function SpotifyPlaylist({ playlistId }: { playlistId: string }) {
-    return (
-      <iframe
-        src={`https://open.spotify.com/embed/playlist/${playlistId}`}
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allow="encrypted-media"
-        className="rounded-lg"
-        style={{
-          border: "none",
-        }}
-      ></iframe>
-    );
-  }
+// export function SpotifyPlaylist({ playlistId }: { playlistId: string }) {
+//     return (
+//       <iframe
+//         src={`https://open.spotify.com/embed/playlist/${playlistId}`}
+//         width="100%"
+//         height="100%"
+//         frameBorder="0"
+//         allow="encrypted-media"
+//         className="rounded-lg"
+//         style={{
+//           border: "none",
+//         }}
+//       ></iframe>
+//     );
+//   }
   
+import { useState, useEffect } from "react";
+
+export function SpotifyPlaylist({ playlistId }: { playlistId: string }) {
+  const [src, setSrc] = useState(`https://open.spotify.com/embed/playlist/${playlistId}`);
+  const [retryCount, setRetryCount] = useState(0);
+
+  useEffect(() => {
+    const handleTimeout = setTimeout(() => {
+      if (retryCount < 3) {
+        setRetryCount((prev) => prev + 1);
+        setSrc(`https://open.spotify.com/embed/playlist/${playlistId}?retry=${retryCount}`);
+      }
+    }, 3000);
+
+    return () => clearTimeout(handleTimeout);
+  }, [retryCount, playlistId]);
+
+  return (
+    <iframe
+      src={src}
+      width="100%"
+      height="100%"
+      frameBorder="0"
+      allow="encrypted-media"
+      className="rounded-lg"
+      style={{ border: "none" }}
+    ></iframe>
+  );
+}
